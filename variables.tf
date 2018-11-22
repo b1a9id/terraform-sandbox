ab9a5887b04a637ee97b05f9f6359c120e7c5c70
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2018-11-22 17:10:10 +0900

    fix backend s3

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ provider "aws" {
   region = "ap-northeast-1"
   profile = "develop-ruchitate"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "バケット名"
+    region = "ap-northeast-1"
+    profile = "develop-ruchitate"
+    key = "terraform.tfstate"
+    encrypt = true
+  }
+}
